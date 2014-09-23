df7257d2e9d9267bae57efaee6564b2bc41df20e
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-09-23 19:26:28 +0200

    initial commit

---
+++ src/NeoApp/Controller/WebController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace NeoApp\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class WebController
+{
+    public function home(Request $request, Application $application)
+    {
+        return $application['twig']->render('index.twig', array(
+
+        ));
+    }
+
+    public function getActor($name, Request $request, Application $application)
+    {
+        $client = $application['neo'];
+        $formatter = $application['formatter'];
+
+        $q = 'MATCH p=(a:Actor { name: {props}.name })-[:ACTS_IN]-() RETURN p';
+        $p = array(
+            'props' => array(
+                'name' => $name
+            )
+        );
+
+        $response = $client->sendCypherQuery($q, $p, null, array('graph'));
+        $result = $formatter->format($response);
+
+        $actors = $result->getNodesByLabel('Actor');
+        $actors = array_shift($actors);
+
+
+
+        return $application['twig']->render('actorShow.twig', array(
+            'actor' => $actors
+        ));
+    }
+
+    public function getMovie($title, Request $request, Application $application)
+    {
+        $client = $application['neo'];
+        $formatter = $application['formatter'];
+
+        $q = 'MATCH p=(m:Movie {title: {props}.title })-[:ACTS_IN]-() RETURN p';
+        $p = array('props' => array(
+            'title' => $title
+        ));
+
+        $response = $client->sendCypherQuery($q, $p, null, array('graph'));
+        $result = $formatter->format($response);
+
+        $movies = $result->getNodesByLabel('Movie');
+        $movie = array_shift($movies);
+        $actors = $movie->getInboundRelationships();
+
+
+        return $application['twig']->render('movieShow.twig', array(
+            'movie' => $movie,
+            'actors' => $actors
+        ));
+    }
+
+    public function actorsListMenu(Request $request, Application $app)
+    {
+        $client = $app['neo'];
+        $query = 'MATCH (n:Actor) RETURN n';
+        $response = $client->sendCypherQuery($query, array(), null, array('graph'));
+        $result = $app['formatter']->format($response);
+
+        $actors = $result->getNodesByLabel('Actor');
+
+        return $app['twig']->render('menu_actors.twig', array(
+            'actors' => $actors
+        ));
+    }
+
+    public function moviesListMenu(Request $request, Application $application)
+    {
+        $client = $application['neo'];
+        $query = 'MATCH (m:Movie) RETURN m';
+
+        $response = $client->sendCypherQuery($query, array(), null, array('graph'));
+        $result = $application['formatter']->format($response);
+
+        $movies = $result->getNodesByLabel('Movie');
+
+        return $application['twig']->render('menu_movies.twig', array(
+            'movies' => $movies
+        ));
+    }
+
+    public function initDb(Request $request, Application $application)
+    {
+        $client = $app['client'];
+        $q = "CREATE (matrix1:Movie { title : 'The Matrix', year : '1999-03-31' })
+CREATE (matrix2:Movie { title : 'The Matrix Reloaded', year : '2003-05-07' })
+CREATE (matrix3:Movie { title : 'The Matrix Revolutions', year : '2003-10-27' })
+CREATE (keanu:Actor { name:'Keanu Reeves' })
+CREATE (laurence:Actor { name:'Laurence Fishburne' })
+CREATE (carrieanne:Actor { name:'Carrie-Anne Moss' })
+CREATE (keanu)-[:ACTS_IN { role : 'Neo' }]->(matrix1)
+CREATE (keanu)-[:ACTS_IN { role : 'Neo' }]->(matrix2)
+CREATE (keanu)-[:ACTS_IN { role : 'Neo' }]->(matrix3)
+CREATE (laurence)-[:ACTS_IN { role : 'Morpheus' }]->(matrix1)
+CREATE (laurence)-[:ACTS_IN { role : 'Morpheus' }]->(matrix2)
+CREATE (laurence)-[:ACTS_IN { role : 'Morpheus' }]->(matrix3)
+CREATE (carrieanne)-[:ACTS_IN { role : 'Trinity' }]->(matrix1)
+CREATE (carrieanne)-[:ACTS_IN { role : 'Trinity' }]->(matrix2)
+CREATE (carrieanne)-[:ACTS_IN { role : 'Trinity' }]->(matrix3)";
+
+        $client->sendCypherQuery($q);
+
+        return $application->redirect('/');
+    }
+}
---
+++ web/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once './../vendor/autoload.php';
+
+
+/**
+ * Neo4j Connection building
+ */
+$url = parse_url(getenv('GRAPHENEDB_URL'));
+$client = new Neoxygen\NeoClient\Client();
+$client->addConnection('default', $url['scheme'], $url['host'], $url['port'], true, $url['user'], $url['password'])
+    ->build();
+$formatter = new Neoxygen\NeoClient\Formatter\ResponseFormatter();
+
+/**
+ * Silex bootstrap
+ */
+$app = new Silex\Application();
+$app['debug'] = true;
+$app['neo'] = $client;
+$app['formatter'] = $formatter;
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../src/views',
+));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->get('/', 'NeoApp\\Controller\\WebController::home')
+    ->bind('homepage');
+$app->get('/actor/{name}', 'NeoApp\\Controller\\WebController::getActor')
+    ->bind('actor');
+$app->get('/menu_actors', 'NeoApp\\Controller\\WebController::actorsListMenu')
+    ->bind('actors_list_menu');
+$app->get('/movie/{title}', 'NeoApp\\Controller\\WebController::getMovie')
+    ->bind('movie');
+$app->get('/menu_movies', 'NeoApp\\Controller\\WebController::moviesListMenu')
+    ->bind('movies_list_menu');
+
+$app->run();
